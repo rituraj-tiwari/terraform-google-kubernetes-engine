38a7fe535fd34bd61cb7d9b19efd16b3e00277e6
ritiwa_acxiom <rituraj.Tiwari@acxiom.com>
2021-10-05 01:25:18 +0530

    tt

--- modules/private-cluster/sa.tf
+++ modules/private-cluster/sa.tf
@@ -46,6 +46,13 @@ resource "google_service_account" "cluster_service_account" {
 resource "google_project_iam_member" "cluster_service_account-log_writer" {
   count   = var.create_service_account ? 1 : 0
   project = google_service_account.cluster_service_account[0].project
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.cluster_service_account[0].email}"
+}
+
+resource "google_project_iam_member" "cluster_service_account-log_writer" {
+  count   = var.create_service_account ? 1 : 0
+  project = google_service_account.cluster_service_account[0].project
   role    = "roles/logging.logWriter"
   member  = "serviceAccount:${google_service_account.cluster_service_account[0].email}"
 }
